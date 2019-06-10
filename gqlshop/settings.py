3a01ee0d70de284098c96ee8148a7f9a8367e596
Vladimir Filonov <vladimir@filonov.dev>
2019-06-10 21:30:18 +0300

    Add ALLOWED_HOSTS to settings

--- gqlshop/settings.py
+++ gqlshop/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
 
 ]
 
+ALLOWED_HOSTS = ["*"]
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
